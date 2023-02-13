748a5fbabc6ba798608afa4dbd4bb528fee8bfc3
GladysKigen <gladys.kigen@student.moringa.com>
2023-02-13 16:38:09 +0300

    completed

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,57 @@
+class Book
+    #attr_accessor :title
+    attr_reader :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    #this is also a setter method,so we need attr_reader
+    def initialize(title)
+        @title = title
+    end
+
+    #def initialize(author)
+       # @author = author
+    #end
+
+    #def initialize(page_count)
+      #  @page_count = page_count
+   # end
+
+   # def initialize(genre)
+     #   @genre = genre
+   # end
+
+
+    #setter
+   # def title(title)
+      #  @title = title
+    #end
+#getter
+    #def title
+       # @title
+    #end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+
+    book = Book.new("And Then There Were None")
+   puts book.title
+    
+
+   book.author = "Agatha Christie"
+   puts book.author
+
+
+   book.page_count = 272
+   puts book.page_count
+
+   book.genre = "Mystery"
+   puts book.genre
+
+   book.turn_page
+
+
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,34 @@
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    attr_reader :brand
+
+   # Shoe .new gets initialized with a brand 
+   #this is a setter too, needs getter/attr reader
+   def initialize(brand)
+    @brand = brand
+   end
+
+   def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+   end
+end
+#Shoe properties has a brand, color,size,material,condition
+shoe = Shoe.new("Nike")
+puts shoe.brand
+
+shoe.color = "red"
+puts shoe.color
+
+shoe.size = 9.5
+puts shoe.size
+
+shoe.material = "suede"
+puts shoe.material
+
+shoe.condition = "tattered"
+puts shoe.condition
+
+shoe.cobble
+puts shoe.condition
